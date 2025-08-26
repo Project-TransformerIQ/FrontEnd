4a29e1411a5fc1f90d8d59b75d32f0502d301f28
Tharushi Karavita <nishelkatharu@gmail.com>
2025-08-26 19:57:07 +0530

    enumbers

---
+++ transformer-frontend/src/constants/enums.js
@@ -0,0 +1,11 @@
+// src/constants/enums.js
+export const IMAGE_TYPES = {
+  BASELINE: "BASELINE",
+  MAINTENANCE: "MAINTENANCE",
+};
+
+export const WEATHER = ["SUNNY", "CLOUDY", "RAINY"];
+
+export const TRANSFORMER_TYPES = ["BULK", "DISTRIBUTION"];
+
+export const STATUS = ["OPEN", "IN_PROGRESS", "CLOSED"];
